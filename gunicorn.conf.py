ac1c3768f25a0afc88c3c064afd7f4fadd7d861f
Ignacio Martinez Tanoira <ignacio.martinez@moorea.io>
2019-12-17 10:38:45 -0300

    fixing values

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -3,7 +3,7 @@ import os
 
 
 bind = "0.0.0.0:8080"
-workers = 1
+workers = multiprocessing.cpu_count() * 2 + 1
 reload = True if os.environ.get("ENV") == "DEV" else False
 errorlog="-"
 accesslog="-"
